e3d5200b4c7ad3712d5a10a4c33b041a9e2201bd
Islam <islamwork15@gmail.com>
2024-12-09 19:45:05 +0300

    update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vite.dev/config/
 export default defineConfig({
   base: '/test-ecosystem/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
   plugins: [react()],
 });
