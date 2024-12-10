7e541585e96f821820c3cff2cbb1f1ec8561fa3b
Islam <islamwork15@gmail.com>
2024-12-10 20:10:49 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
   plugins: [react()],
 });
